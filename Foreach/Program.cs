aa3ee7dcfc2ec986bbb1ddc18154c8f11e53156e
arielneomatrix <68985117+arielneomatrix@users.noreply.github.com>
2025-08-08 01:49:41 -0300

    Agregar archivos de proyecto.

---
+++ Foreach/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] Nombres = { "gikbert", "jorge", "juno", "Karina", "noelia" };
+            string[] Apellidos = { "Vargas", "Lara", "Ramirez", "fernandez", "Rodriguez" };
+
+            foreach (string Usuario in Nombres)
+        
+            {
+                Console.WriteLine("El nombre es: " + Usuario);
+            }
+
+            {
+                Console.WriteLine("El apellido es: " + Apellidos);
+            }
+            
+            Console.ReadKey();
+        }
+
+    }
+}
